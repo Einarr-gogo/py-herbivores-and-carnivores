168e65c4fbad08df956bfc6946ccc4ced991b004
Oleksandr <aleksandrpaskoo22@gmail.com>
2025-01-09 19:32:05 +0100

    herbivores-and-carnivores implement

--- app/main.py
+++ app/main.py
@@ -1,1 +1,34 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str,
+                 health: int = 100,
+                 hidden: bool = False) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        self.__class__.alive.append(self)
+
+    def take_damage(self, amount: int) -> None:
+        self.health = self.health - amount
+        if self.health <= 0:
+            self.__class__.alive.remove(self)
+
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, priest: Animal) -> None:
+        if isinstance(priest, Herbivore):
+            if not priest.hidden:
+                priest.take_damage(50)
